709aa58b636d5e323558aae4a834d18a0784885d
bubbless2005 <pshindekar065.btech2023@me.nitrr.ac.in>
2026-08-14 17:21:53 +0530

    Added analysis_queries.sql

--- data/sql /analysis_queries.sql
+++ data/sql /analysis_queries.sql
@@ -1,1 +1,41 @@
+-- Transaction Success Rate
 
+SELECT
+COUNT(CASE WHEN Status='Success' THEN 1 END)*100.0
+/
+COUNT(*) AS Success_Rate
+FROM Transactions;
+
+------------------------------------------------
+
+-- Top Failure Reasons
+
+SELECT
+Failure_Reason,
+COUNT(*) AS Failure_Count
+FROM Transactions
+WHERE Status='Failed'
+GROUP BY Failure_Reason
+ORDER BY Failure_Count DESC;
+
+------------------------------------------------
+
+-- Merchant Category Analysis
+
+SELECT
+Merchant_Category,
+COUNT(*) AS Transactions
+FROM Transactions
+GROUP BY Merchant_Category
+ORDER BY Transactions DESC;
+
+------------------------------------------------
+
+-- State-wise Transactions
+
+SELECT
+State,
+COUNT(*) AS Transaction_Count
+FROM Transactions
+GROUP BY State
+ORDER BY Transaction_Count DESC;
