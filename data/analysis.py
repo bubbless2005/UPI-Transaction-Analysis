8934d65cc3474925c1ee7c9ac1cbab8c59acb974
bubbless2005 <pshindekar065.btech2023@me.nitrr.ac.in>
2026-08-14 17:42:19 +0530

    Update analysis.py

--- data/analysis.py
+++ data/analysis.py
@@ -1,1 +1,17 @@
+import pandas as pd
 
+df = pd.read_csv("data/upi_transaction_insights_dataset.csv")
+
+print("Total Transactions:", len(df))
+
+print("\nTransaction Success Rate:")
+print(df["is_successful"].value_counts(normalize=True)*100)
+
+print("\nPayment Mode Distribution:")
+print(df["payment_mode"].value_counts())
+
+print("\nMerchant Category Distribution:")
+print(df["merchant_category"].value_counts())
+
+print("\nAverage Transaction Amount:")
+print(df["amount"].mean())
